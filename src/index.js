cff1e420407ff90ab46bd587eaa9392ad2456fc2
Admin <d>
2020-03-04 00:46:54 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =
+function check(str, bracketsConfig) {
+    let arr = str.split(''),
+        tempArr = [],
+        closingBracket = [],
+        falseResult = 0;
+
+    arr.forEach(bracket => {
+        if (tempArr.length === 0 && bracketsConfig.some((elem) => {
+            if (bracket === elem[0]) {
+                closingBracket.push(elem[1])
+            }
+            return bracket === elem[0]
+        })) tempArr.push(bracket);
+        else if (tempArr.length !== 0) {
+            if (bracket === closingBracket[closingBracket.length - 1]) {
+                tempArr.pop();
+                closingBracket.pop();
+            } else if (bracketsConfig.some((elem) => {
+                if (bracket === elem[0]) {
+                    closingBracket.push(elem[1]);
+                }
+                return bracket === elem[0]
+            })) tempArr.push(bracket);
+            else falseResult = 1;
+
+        } else falseResult = 1
+    });
+return (falseResult === 0 && tempArr.length === 0)
 }
+
+
